42cb42ce324839e44051d42e4d3ac0bb3dbcf7ce
Siavash Soleymani <siavash.soleymani@apple.com>
2022-07-25 11:48:44 -0400

    add object doc and package ddoc

--- R/matches.R
+++ R/matches.R
@@ -1,3 +1,15 @@
+#' Makes a standard dataframe for league data
+#'
+#' @param data_engsoc dataframe from engsoccerdata package
+#' @param country country `character` specifies the league.
+#'  one of spain, italy, germany, england, holland
+#'
+#' @return  a tibble with columns `country`, `date`, `season`, `tier`, `home`,
+#'    `visitor`, `goals_home`, `goals_visitor`.
+#' @export
+#'
+#' @examples
+#' uss_make_matches(engsoccerdata::england, "England")
 uss_make_matches <- function(data_engsoc, country) {
   result <-
     data_engsoc %>%
