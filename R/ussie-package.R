ade7d52648ad69f4a74998f422bd5488668ce674
Siavash Soleymani <siavash.soleymani@apple.com>
2022-07-26 09:33:28 -0400

    fix the .data and .env

--- R/ussie-package.R
+++ R/ussie-package.R
@@ -4,6 +4,8 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @importFrom rlang .data
+#' @importFrom rlang .env
 #' @importFrom tibble tibble
 ## usethis namespace: end
 NULL
