3676da83d02ff528c0e6b7a290d8434281c358cf
Siavash Soleymani <siavash.soleymani@apple.com>
2022-07-26 14:21:09 -0400

    validate and filtering added

--- tests/testthat/test-matches.R
+++ tests/testthat/test-matches.R
@@ -1,10 +1,11 @@
 test_that("uss_make_matches works", {
-  
+
   italy <- uss_make_matches(engsoccerdata::italy, "Italy")
-  
+
   expect_true(tibble::is_tibble(italy))
   expect_named(italy, cols_matches())
   expect_identical(unique(italy$country), "Italy")
 
   expect_s3_class(italy$tier, "factor")
+  # expect_snapshot(dplyr::glimpse(italy))
 })
